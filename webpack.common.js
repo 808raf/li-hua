dc94cf46286adfce06713104c9db674f2f2a1703
808raf <102700868+808raf@users.noreply.github.com>
2024-04-10 10:17:59 -1000

    Split webpack into dev and prod config

--- webpack.common.js
+++ webpack.common.js
@@ -3,13 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    index: "./src/index.js",
+    app: "./src/index.js",
   },
-  mode: "production",
-  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Li Hua",
+      title: "Li Hua Tea",
       template: "./src/index.html",
     }),
   ],
@@ -23,14 +21,6 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
       },
     ],
   },
